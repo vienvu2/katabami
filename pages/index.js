168f0b1c269fc2fe07b9c9dfca81e60f6ffa9d2e
VienVu <vienvu@email.com>
2023-04-08 11:11:09 +0700

    SOME CSS

--- pages/index.js
+++ pages/index.js
@@ -22,13 +22,14 @@ export default function Home() {
                 không chỉ tự hào bởi sở hữu hệ thống phòng khám trải dài từ Bắc tới Nam.</p>
             </div>
           </div>
-          <div className="row">
+          <div className="row mb-4">
             {[1, 2, 3].map(a => <div className="col-md-4 col-12" key={a}>
               <div className="product-item">
                 <img src="/images/banner1.jpeg" />
-
-                <h5>PHỤC HỒI VÀ NGĂN CHẶN QUÁ
-                  TRÌNH THOÁI HOÁ KHỚP</h5>
+                <a>
+                  <h5>PHỤC HỒI VÀ NGĂN CHẶN QUÁ
+                    TRÌNH THOÁI HOÁ KHỚP</h5>
+                </a>
 
                 <p>Chiropractic không phải là một phương pháp
                   xuất hiện gần đây mà thực sự là một chuyên
@@ -57,13 +58,15 @@ export default function Home() {
                 không chỉ tự hào bởi sở hữu hệ thống phòng khám trải dài từ Bắc tới Nam.</p>
             </div>
           </div>
-          <div className="row">
+          <div className="row mb-4">
             {[1, 2, 3].map(a => <div className="col-md-4 col-12" key={a}>
               <div className="product-item">
                 <img src="/images/banner1.jpeg" />
 
-                <h5>PHỤC HỒI VÀ NGĂN CHẶN QUÁ
-                  TRÌNH THOÁI HOÁ KHỚP</h5>
+                <a>
+                  <h5>PHỤC HỒI VÀ NGĂN CHẶN QUÁ
+                    TRÌNH THOÁI HOÁ KHỚP</h5>
+                </a>
 
                 <p>Chiropractic không phải là một phương pháp
                   xuất hiện gần đây mà thực sự là một chuyên
@@ -88,26 +91,25 @@ export default function Home() {
           <div className="row mb-4">
             <div className="col-md-12">
               <h3 className="title">Đội ngũ bác sĩ</h3>
-
             </div>
           </div>
-          <div className="row">
+          <div className="row mb-4">
             {[1, 2, 3].map(a => <div className="col-md-4 col-12" key={a}>
               <div className="product-item">
                 <img src="/images/banner1.jpeg" />
+                <a>
+                  <h5>DR. HO NGOC HUY</h5>
+                </a>
 
-                <h5>PHỤC HỒI VÀ NGĂN CHẶN QUÁ
-                  TRÌNH THOÁI HOÁ KHỚP</h5>
-
-                <p>Chiropractic không phải là một phương pháp
-                  xuất hiện gần đây mà thực sự là một chuyên
-                  ngành với nguồn gốc (...)</p>
+                <p>Hoàn thành 4 năm đại học đại cương, 2 năm
+                  chuyên sâu về khoa học và 4 năm chuyên
+                  ngành Chiropractic (...)</p>
               </div>
             </div>)}
           </div>
 
           <div className="text-center">
-            <button className="btn btn-primary">TẤT CẢ DỊCH VỤ</button>
+            <button className="btn btn-primary">XEM TẤT CẢ</button>
           </div>
         </div>
       </section>
@@ -124,32 +126,34 @@ export default function Home() {
       <section className="home-why py-8">
         <div className="container">
           <div className="row">
-            <div className="col-md-4 col-12">
+            <div className="col-md-4 col-12 mb-4">
               <h3 className="title">Tại sao nên lựa chọn  Katabami Clinic ?</h3>
             </div>
-            <div className="col-md-8 col-12">
+            <div className="col-md-8 col-12  mb-4">
               <p className="desc">Trung tâm điều trị cơ xương khớp chuẩn Mỹ hàng đầu tại Việt Nam, Katabami Clinic
                 không chỉ tự hào bởi sở hữu hệ thống phòng khám trải dài từ Bắc tới Nam.</p>
-
+            </div>
+            <div className="col-md-4 col-12" />
+            <div className="col-md-8 col-12 mb-4">
               <div className="row">
                 <div className="col-md-3">
-                  <h3>15+</h3>
-                  <p>Hơn 15 năm trong lĩnh
+                  <h3 className="title-big">15+</h3>
+                  <p className="desc-sm">Hơn 15 năm trong lĩnh
                     vực xương khớp</p>
                 </div>
                 <div className="col-md-3">
-                  <h3>15+</h3>
-                  <p>Hơn 15 năm trong lĩnh
+                  <h3 className="title-big">15+</h3>
+                  <p className="desc-sm">Hơn 15 năm trong lĩnh
                     vực xương khớp</p>
                 </div>
                 <div className="col-md-3">
-                  <h3>15+</h3>
-                  <p>Hơn 15 năm trong lĩnh
+                  <h3 className="title-big">15+</h3>
+                  <p className="desc-sm">Hơn 15 năm trong lĩnh
                     vực xương khớp</p>
                 </div>
                 <div className="col-md-3">
-                  <h3>15+</h3>
-                  <p>Hơn 15 năm trong lĩnh
+                  <h3 className="title-big">15+</h3>
+                  <p className="desc-sm">Hơn 15 năm trong lĩnh
                     vực xương khớp</p>
                 </div>
               </div>
@@ -157,7 +161,7 @@ export default function Home() {
           </div>
 
           <div className="row">
-            {[1, 2, 3].map(a => <div className="col-md-4 col-12" key={a}>
+            {[1, 2, 3, 4].map(a => <div className="col-md-3 col-12" key={a}>
               <div className="product-item">
                 <img src="/images/banner1.jpeg" />
 
@@ -181,39 +185,69 @@ export default function Home() {
       <section className="home-news py-8 bg-gray">
         <div className="container">
           <div className="row">
-            <div className="col-md-3 col-12">
+            <div className="col-md-3 col-12 mb-4">
               <h3 className="title">Tin tức</h3>
 
             </div>
-            <div className="col-md-9 col-12">
+            <div className="col-md-9 col-12 mb-4">
               <Tabs />
             </div>
+          </div>
+          <div className="row">
+            {[1, 2, 3, 4].map(a => <div className="col-md-3 col-12" key={a}>
+              <div className="product-item">
+                <img src="/images/banner1.jpeg" />
+
+             
+                <h5>Bệnh viêm khớp ngày càng
+                    trẻ hóa, giải pháp nào điều
+                    trị sớm?</h5>
+
+                <p>Chiropractic không phải là một phương pháp
+                  xuất hiện gần đây mà thực sự là một chuyên
+                  ngành với nguồn gốc (...)</p>
+              </div>
+            </div>)}
           </div>
         </div>
       </section>
 
-      <section className="home-video py-8">
+      <section className="home-video pt-8 pb-2">
         <div className="container">
           <div className="row">
-            <div className="col-md-3 col-12">
+            <div className="col-md-3 col-12 mb-4">
               <h3 className="title">Video</h3>
 
             </div>
-            <div className="col-md-9 col-12">
+            <div className="col-md-9 col-12 mb-4">
               <Tabs />
             </div>
+          </div>
+          <div className="row">
+            {[1, 2, 3, 4].map(a => <div className="col-md-3 col-12 mb-4" key={a}>
+              <div className="product-item">
+                <img src="/images/banner1.jpeg" />
+
+                <a>
+
+                  <h5>Bệnh viêm khớp ngày càng
+                    trẻ hóa, giải pháp nào điều
+                    trị sớm?</h5>
+                </a>
+
+                <p>Chiropractic không phải là một phương pháp
+                  xuất hiện gần đây mà thực sự là một chuyên
+                  ngành với nguồn gốc (...)</p>
+              </div>
+            </div>)}
+          </div>
+
+          <div className="text-center">
+            <button className="btn btn-primary">XEM TẤT CẢ</button>
           </div>
         </div>
       </section>
 
-
-      <section className="home-why  py-8">
-        <div className="container">
-          <h3 className="title">Tại sao nên lựa chọn
-            Katabami Clinic ?</h3>
-        </div>
-
-      </section>
 
       <section className="home-thanks  pt-8">
         <div className="container mb-8">
@@ -230,17 +264,17 @@ export default function Home() {
           {[1, 2, 3, 4, 5, 6, 7, 8].map(item => <div
             className="image-grid-item"
             key={item} style={{ backgroundImage: `url('/images/banner1.jpeg')` }}>
-              <div className="image-grid-content">
-            <p>
-              Đến Katabami Clinic điều trị, tôi
-              cảm thấy rất yên tâm vì ở đây trị
-              bệnh không phẫu thuật, không
-              cần uống thuốc, an toàn lắm.
-            </p>
-            <strong> Phạm Ngọc Lan</strong>
-            <p>
-              (Nội trợ)
-            </p>
+            <div className="image-grid-content">
+              <p>
+                Đến Katabami Clinic điều trị, tôi
+                cảm thấy rất yên tâm vì ở đây trị
+                bệnh không phẫu thuật, không
+                cần uống thuốc, an toàn lắm.
+              </p>
+              <strong> Phạm Ngọc Lan</strong>
+              <p>
+                (Nội trợ)
+              </p>
             </div>
           </div>)}
 
--- src/layouts/contact.js
+++ src/layouts/contact.js
@@ -1,30 +1,30 @@
 export default function Contact() {
-    return <div className="contac">
+    return <div className="contact py-8">
         <div className="container">
 
-            <div className="row">
+            <div className="row align-middle">
                 <div className="col-md-6">
-                    <h2>Liên hệ tư vấn</h2>
+                    <h2 className="mb-1">Liên hệ tư vấn</h2>
 
-                    <p>Vui lòng cung cấp thông tin,
+                    <p className="mb-2">Vui lòng cung cấp thông tin,
                         bộ phận CSKH sẽ liên hệ lại trong thời gian sớm nhất</p>
 
-                    <a>028 345 678 9</a>
+                    <a className="phone">028 345 678 9</a>
                 </div>
-                <div className="col-md-6">
+                <div className="col-md-6 pt-4">
 
                     <form className="custom-form">
                         <div className="row">
-                            <div className="col-md-6">
+                            <div className="col-md-6 mb-2">
                                 <input placeholder="Họ và tên" />
                             </div>
-                            <div className="col-md-6">
+                            <div className="col-md-6  mb-2">
                                 <input placeholder="Số điện thoại" />
                             </div>
-                            <div className="col-md-12">
+                            <div className="col-md-12  mb-4">
                                 <input placeholder="Nội dung" />
                             </div>
-                            <div className="col-md-12">
+                            <div className="col-md-12  mb-2">
                                 <button className="btn">Gửi</button>
                             </div>
                         </div>
